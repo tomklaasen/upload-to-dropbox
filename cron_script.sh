d4adbb561cb0518afa8d1cb9b669f08dbe531fd9
Tom Klaasen <tom@tomklaasen.be>
2019-10-20 17:22:22 +0200

    added cron script

---
+++ cron_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PIDFILE=/home/pi/upload-to-dropbox.pid
+
+
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    # echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      # echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    # echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+python /home/pi/upload-to-dropbox/main.py
+
+rm $PIDFILE
